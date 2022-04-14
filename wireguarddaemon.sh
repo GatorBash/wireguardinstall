031fec4605ac7b9a07f2be5ea9a8f9674bc08bc7
GatorBash <95302944+GatorBash@users.noreply.github.com>
2022-04-14 13:16:47 -0500

    Add files via upload

---
+++ wireguarddaemon.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Hello!"
+sleep 3
+echo "Lets set up the daemon for you."
+sleep 3
+echo "Do you want to turn the daemon on, off, or exit the script?"
+echo "1) on"
+echo "2) off"
+echo "3) exit"
+read r
+if [[ "$r" = [1-3] ]]
+then
+	case $r
+		1)
+			sudo systemctl enable wg-quick@wg0
+			echo "wireguard enabled"
+			;;
+
+		2)
+			sudo systemctl disable wg-quick@wg0
+			echo "wireguard disabled"
+			;;
+
+		3)
+			exit
+			;;
+	esac
