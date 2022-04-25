8e6df066866ba4f40cb077cadafd9e000071f86f
GatorBash <95302944+GatorBash@users.noreply.github.com>
2022-04-25 16:45:06 -0500

    Update wireguardinstall.sh
    added some questions for the user to fill out to put into variables for the configuration file and commented out the sed command. I spent too long looking into it to get rid of it. using "cat privatekey" function instead for now.  Still need more testing to make sure i get a consistent results.

--- wireguardinstall.sh
+++ wireguardinstall.sh
@@ -26,16 +26,22 @@ cd /etc/wireguard
 umask 077
 wg genkey | tee privatekey | wg pubkey > publickey
 touch wg0.conf
+echo "what is the ip of your server?"
+read server
+echo "what port are you using for your server?"
+read port
+echo "What IP are you going to use for your client?"
+read client
 echo "[Interface]" >> wg0.conf
-echo "PrivateKey = " >> wg0.conf
-echo "Address = " >> wg0.conf
+echo "PrivateKey = $key" >> wg0.conf
+echo "Address = $client" >> wg0.conf
 echo "[Peer]" >> wg0.conf
 echo "PublicKey = <server public key" >> wg0.conf
 echo "AllowedIPs = 0.0.0.0/0" >> wg0.conf
-echo "Endpoint = <server public ip:port>" >> wg0.conf
-echo "PersistentKeepalive = 25"
+echo "Endpoint = $server:$port>" >> wg0.conf
+echo "PersistentKeepalive = 25" >> wg0.conf
 #the sed command is ment to add in your private key to the config file but it has been hit or miss
-sed -i "/$old_key/c\\$old_key$key" wg0.conf
+#sed -i "/$old_key/c\\$old_key$key" wg0.conf
 else
 	echo "sign into root using 'sudo -i' and run the script again."
 	exit
