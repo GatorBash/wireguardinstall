f32d2dc4c56bb7f3efc7fa78cde4e1860040a046
GatorBash <95302944+GatorBash@users.noreply.github.com>
2022-04-26 20:48:41 -0500

    Update wireguardinstall.sh

--- wireguardinstall.sh
+++ wireguardinstall.sh
@@ -1,51 +1,66 @@
 #!/bin/bash
 
 key=$(cat privatekey)
-old_key='PrivateKey = '
+#old_key='PrivateKey = '
 dir='/etc/wireguard'
 
 echo "Are you logged in as root? y or n "
-read r
-if [ $r = y ]
+read -r r
+if [ "$r" = y ]
 then
-echo "ok we are gonna make this nice"
-sleep 3
-echo "and...."
-sleep 3
-echo "easy"
-sleep 5
-echo "First we're going to update and install some things"
-echo "Get ready to put in your password"
-sudo apt update -y
-wait
-sudo apt install wireguard wireguard-dkms wireguard-tools -y
-wait
-echo "Hopefully that didn't error out"
-sleep 5
-cd /etc/wireguard
-umask 077
-wg genkey | tee privatekey | wg pubkey > publickey
-touch wg0.conf
-echo "what is the ip of your server?"
-read server
-echo "what port are you using for your server?"
-read port
-echo "What IP are you going to use for your client?"
-read client
-echo "[Interface]" >> wg0.conf
-echo "PrivateKey = $key" >> wg0.conf
-sleep 1
-echo "Address = $client" >> wg0.conf
-sleep 1
-echo "[Peer]" >> wg0.conf
-echo "PublicKey = <server public key>" >> wg0.conf
-echo "AllowedIPs = 0.0.0.0/0" >> wg0.conf
-echo "Endpoint = $server:$port" >> wg0.conf
-sleep 1
-echo "PersistentKeepalive = 25" >> wg0.conf
+	echo "ok we are gonna make this nice"
+	sleep 3
+	echo "and...."
+	sleep 3
+	echo "easy"
+	sleep 5
+	echo "First we're going to update and install some things"
+	sudo apt update -y
+	wait
+	sudo apt install wireguard wireguard-dkms wireguard-tools -y
+	wait
+	clear
+	echo "Hopefully that didn't error out"
+	sleep 5
+	cd /etc/wireguard
+	umask 077
+	wg genkey | tee privatekey | wg pubkey > publickey
+	touch wg0.conf
+	echo "what is the ip of your server?"
+	read -r server
+	echo "what port are you using for your server?"
+	read -r port
+	echo "What IP are you going to use for your client?"
+	read -r client
+	echo "[Interface]" >> wg0.conf
+	echo "PrivateKey = $key" >> wg0.conf
+	sleep 1
+	echo "Address = $client" >> wg0.conf
+	sleep 1
+	echo "Do you have the publickey file? y/n"
+	read -r rep
+	if [ "$rep" = n ]
+	then
+		echo "[Peer]" >> wg0.conf
+		echo "PublicKey = <server public key>" >> wg0.conf
+		echo "AllowedIPs = 0.0.0.0/0" >> wg0.conf
+		echo "Endpoint = $server:$port" >> wg0.conf
+		sleep 1
+		echo "PersistentKeepalive = 25" >> wg0.conf
+	else
+		echo "Type in the absolute path to the public key file"
+		read -r pub
+		pubkey=$(cat "$pub")
+		echo "[Peer]" >> wg0.conf
+		echo "PublicKey = "$pubkey"" >> wg0.conf
+		echo "AllowedIPs = 0.0.0.0/0" >> wg0.conf
+		echo "Endpoint = $server:$port" >> wg0.conf
+		sleep 1
+		echo "PersistentKeepalive = 25" >> wg0.conf
+	fi
 #the sed command is ment to add in your private key to the config file but it has been hit or miss
 #sed -i "/$old_key/c\\$old_key$key" wg0.conf
 else
 	echo "sign into root using 'sudo -i' and run the script again."
-	exit
+	exit 0
 fi
