08d39cfc2cd22c1d37cc58d86bdf0ff31e99c24a
GatorBash <95302944+GatorBash@users.noreply.github.com>
2022-04-28 16:10:47 -0500

    Add files via upload

---
+++ fullwginstall.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#this ensures the user is root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root; run \"sudo -i\" this will log you into root." 
+   exit 1
+fi
+chomod +x *
+echo "Are you running this on the server or the client?"
+read -r dev
+if [ "$dev" == client ]
+then
+	echo "Ok we will just run a couple of scripts real quick."
+	./wireguardinstall.sh
+	wait
+	./wireguarddaemon.sh
+	wait
+	echo "Ok go back and make sure everything is in the right place."
+	echo "GLHF"
+elif [ "$dev" == server ]
+then
+	echo "Ok we are just going to run a couple of scripts."
+	./wireguardinstallserver.sh
+	wait
+	./wireguardpeers.sh
+	wait
+else
+	echo "Fully type out client or server"
+fi
